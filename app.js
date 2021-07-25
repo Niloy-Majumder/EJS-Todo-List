1ae8df25996a8fc41dcb15c69eeb95e33fca09fd
Niloy-Majumder <niloymajumder.98@gmail.com>
2021-07-25 10:35:45 +0530

    Third commit

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({extended:true}));
 
+app.set("viewengine", "ejs");
+
+
+
+
 app.get("/", (req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
