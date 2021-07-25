2de30c0d07c58c74fbfe618d834dd0b5da806c4c
Niloy-Majumder <niloymajumder.98@gmail.com>
2021-07-25 15:38:14 +0530

    Full webpage of todo list app

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require("express");
 
-
 const app = express();
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
 
-app.set("viewengine", "ejs");
+var newitems = ["Buy Food", "Eat Food"];
+var today = new Date();
+var currentDay = today.getDay();
+var day = today.toLocaleDateString("default", {
+  weekday: "long"
+});
 
+app.get("/", (req, res) => {
+  res.render("lists", {
+    renderday: day,
+    item: newitems
+  });
+});
 
+app.post("/", (req, res) => {
+  var newitem = req.body.listitem;
+  newitems.push(newitem);
+  res.redirect("/");
+});
 
-
-app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/index.html");
-})
-
-
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server running at port 3000");
+});
